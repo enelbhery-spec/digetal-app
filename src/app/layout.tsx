3ec4328329499cb9dc9a18ec14794fd076047e2a
Ahmed Elbhery <ahmed@gmail.com>
2026-01-01 11:02:57 +0200

    change layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,7 +53,7 @@ export default function RootLayout({
       <head>
         {/* الطريقة الصحيحة لإضافة سكريبت Adsterra في Next.js */}
         <Script
-          src="https://pl28375236.effectivegatecpm.com/68452107c89d06df62ec5e7bed215ec8/invoke.js"
+          src="https://pl28375284.effectivegatecpm.com/a2/d8/f9/a2d8f911d27d875162419ab2c20d9e9b.js"
           strategy="afterInteractive"
           data-cfasync="false"
           async
@@ -62,7 +62,7 @@ export default function RootLayout({
       <body className="min-h-screen flex flex-col bg-gray-50 text-gray-800 font-sans antialiased">
 
         {/* حاوية الإعلان: يفضل وضعها في مكان استراتيجي، هنا ستظهر في أعلى الصفحة */}
-
+         <div id="container-a2d8f911d27d875162419ab2c20d9e9b"></div>
 
         <RegisterSW />
         <Header />
