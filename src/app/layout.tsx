f096abc544dd99363872aa8570187d72ce79a13d
Ahmed Elbhery <ahmed@gmail.com>
2026-02-03 06:52:49 +0200

    add srtipt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -111,13 +111,18 @@ export default function RootLayout({
             });
           `}
         </Script>
-        <script type="text/javascript">
-       (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "vbff8ppfd2");
-     </script>
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `
+(function(c,l,a,r,i,t,y){
+c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "vbff8ppfd2");
+      `,
+    }}
+  />
+t>
       </head>
 
       <body className="min-h-screen flex flex-col bg-gray-50 text-gray-800 antialiased">
