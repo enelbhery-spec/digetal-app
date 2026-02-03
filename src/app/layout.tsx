65c049fc5a0abef5e04814780ec9f7d83e664d2d
Ahmed Elbhery <ahmed@gmail.com>
2026-02-03 06:46:31 +0200

    script for ms

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -111,6 +111,13 @@ export default function RootLayout({
             });
           `}
         </Script>
+        <script type="text/javascript">
+       (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "vbff8ppfd2");
+     </script>
       </head>
 
       <body className="min-h-screen flex flex-col bg-gray-50 text-gray-800 antialiased">
