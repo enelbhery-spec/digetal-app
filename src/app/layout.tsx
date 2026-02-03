068a75fcfc11da5bcb3261d75d4908ddd17d456e
Ahmed Elbhery <ahmed@gmail.com>
2026-02-03 06:56:56 +0200

    add scripet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -111,18 +111,17 @@ export default function RootLayout({
             });
           `}
         </Script>
-  <script
-    dangerouslySetInnerHTML={{
-      __html: `
+{/* Microsoft Clarity */}
+<Script id="clarity" strategy="afterInteractive">
+  {`
 (function(c,l,a,r,i,t,y){
 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
 })(window, document, "clarity", "script", "vbff8ppfd2");
-      `,
-    }}
-  />
-t>
+  `}
+</Script>
+>
       </head>
 
       <body className="min-h-screen flex flex-col bg-gray-50 text-gray-800 antialiased">
