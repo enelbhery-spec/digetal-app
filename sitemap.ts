d3543331d6d01a4eb9a5d2c0a9a3e5d3fc75e114
Ahmed Elbhery <ahmed@gmail.com>
2026-03-12 02:55:51 +0200

    fix sitemap location

--- sitemap.ts
+++ sitemap.ts
@@ -1,47 +1,40 @@
-import { MetadataRoute } from "next";
-import { createClient } from "@supabase/supabase-js";
+import { MetadataRoute } from "next"
+import { createClient } from "@supabase/supabase-js"
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+)
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
-  const baseUrl = "https://www.extracode.online";
+  const baseUrl = "https://www.extracode.online"
 
   const { data: products } = await supabase
     .from("products")
-    .select("slug,country");
+    .select("slug,country")
 
-  const urls: MetadataRoute.Sitemap = [];
+  const urls: MetadataRoute.Sitemap = [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/eg`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/sa`,
+      lastModified: new Date(),
+    }
+  ]
 
-  // الصفحة الرئيسية
-  urls.push({
-    url: baseUrl,
-    lastModified: new Date(),
-  });
+  products?.forEach((product) => {
+    urls.push({
+      url: `${baseUrl}/${product.country}/product/${product.slug}`,
+      lastModified: new Date(),
+    })
+  })
 
-  // صفحات الدول
-  urls.push({
-    url: `${baseUrl}/eg`,
-    lastModified: new Date(),
-  });
-
-  urls.push({
-    url: `${baseUrl}/sa`,
-    lastModified: new Date(),
-  });
-
-  // صفحات المنتجات
-  if (products) {
-    products.forEach((product) => {
-      urls.push({
-        url: `${baseUrl}/${product.country}/product/${product.slug}`,
-        lastModified: new Date(),
-      });
-    });
-  }
-
-  return urls;
+  return urls
 }
