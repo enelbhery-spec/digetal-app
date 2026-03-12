130ac238c98354502c3c94d279c1736b9d3020aa
Ahmed Elbhery <ahmed@gmail.com>
2026-03-13 00:52:57 +0200

    add artucaks

--- src/app/[country]/page.tsx
+++ src/app/[country]/page.tsx
@@ -88,7 +88,6 @@ export default async function CountryPage({ params }: Props) {
     .from("articles")
     .select("title, slug, created_at")
     .order("created_at", { ascending: false })
-    .limit(4)
 
   return (
 
