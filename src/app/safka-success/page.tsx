7a34b4a4500c60b14acc5e81df145d19dbd701ee
Ahmed Elbhery <ahmed@gmail.com>
2026-05-21 23:24:10 +0300

    update

---
+++ src/app/safka-success/page.tsx
@@ -0,0 +1,9 @@
+export default function SafkaSuccessPage() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>تم الربط بنجاح!</h1>
+      <p>لقد تم ربط متجرك بمنصة صفقة بنجاح، يمكنك الآن إدارة منتجاتك.</p>
+      <a href="/dashboard">العودة للوحة التحكم</a>
+    </div>
+  );
+}
